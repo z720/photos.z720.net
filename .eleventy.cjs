ce7d03ce01198a1a4356120c9ef9bcaee84e2a2e
z720 <Seb@z720.net>
2024-11-17 17:01:45 +0100

    v1.2.0 : bump 11ty 3.0 (#10)
    * Switch to GITHUB_TOKEN
    * Remove scheduling
    * Split audit
    * chore: exclude useless docker content
    * chore: update dependancies
    * chore: upgrade 11ty 3.0.0

--- .eleventy.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = function(eleventyConfig) {
-  let today = new Date();
-  eleventyConfig.addPassthroughCopy("assets/");
-  eleventyConfig.addGlobalData("today", `${today.toLocaleDateString('fr')}`);
-  // Return your Object options:
-  return {
-    jsDataFileSuffix: ".11tydata.cjs"
-  }
-};
