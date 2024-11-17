97bc3c552841e49b2492bc313b4d34ad05197a8d
z720 <Seb@z720.net>
2024-11-17 14:00:31 +0000

    chore: upgrade 11ty 3.0.0

---
+++ .eleventy.js
@@ -0,0 +1,14 @@
+// import { I18nPlugin, RenderPlugin, HtmlBasePlugin } from "@11ty/eleventy";
+import mustachePlugin from "@11ty/eleventy-plugin-mustache";
+
+export default function (eleventyConfig) {
+// module.exports = function(eleventyConfig) {
+	let today = new Date();
+	eleventyConfig.addPlugin(mustachePlugin);
+    eleventyConfig.addPassthroughCopy("assets/");
+    eleventyConfig.addGlobalData("today", `${today.toLocaleDateString('fr')}`);
+   // Return your Object options:
+    return {
+      jsDataFileSuffix: ".11tydata.js"
+    }
+};
