1937b6c09d4a4b1042add77beea286132c260848
z720 <seb@z720.net>
2026-02-07 16:52:41 +0000

    fix: build with secrets

--- .eleventy.js
+++ .eleventy.js
@@ -5,10 +5,11 @@ export default function (eleventyConfig) {
 // module.exports = function(eleventyConfig) {
 	let today = new Date();
 	eleventyConfig.addPlugin(mustachePlugin);
-    eleventyConfig.addPassthroughCopy("assets/");
-    eleventyConfig.addGlobalData("today", `${today.toLocaleDateString('fr')}`);
-   // Return your Object options:
-    return {
-      jsDataFileSuffix: ".11tydata.js"
-    }
+  eleventyConfig.addPassthroughCopy("assets/");
+  eleventyConfig.addGlobalData("today", `${today.toLocaleDateString('fr')}`);
+  // Return your Object options:
+  eleventyConfig.setDataFileSuffixes([".11tydata"]);
+  // return {
+  //   jsDataFileSuffix: ".11tydata.js"
+  // }
 };
--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,14 @@
-FROM node as builder
+FROM node AS builder
 WORKDIR /src
 COPY . /src
-ARG FLICKR_API_KEY
+#ARG FLICKR_API_KEY
 ARG FLICKR_COLLECTION
 ARG FLICKR_USER
-RUN npm ci \
-  && npm run generate:data \
-  && npm run generate:site
+RUN npm ci
+RUN --mount=type=secret,id=FLICKR \
+    source /run/secrets/FLICKR; \ 
+    npm run generate:data
+RUN npm run generate:site
 
 FROM ghcr.io/z720/lighttpd:develop
 COPY --from=builder /src/_site/ /var/www/html
